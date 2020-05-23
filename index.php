f3d1ea3632e1c025588a72949f6287c8efab9b75
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-23 17:12:28 -0400

    capturando la url metodo get

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ print("Hola Mari :*");
 
 if($_SERVER['REQUEST_METHOD']=='GET'){
     echo "GET";
+
+    $urlRoute = $_GET['url'];
+    print($urlRoute);
     http_response_code(200);
 } else if($_SERVER['REQUEST_METHOD']=='POST'){
     echo "POST";
