3c649383760c475ba28ffb2788f2d3e2285d9012
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-23 16:49:54 -0400

    uso de metodos get post y default

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-    require_once('db.php');
-    $query = "select * from persona";
-    $resultado = obtenerRegistros($query);
-    print_r($resultado);
+    # require_once('db.php');
+    # $query = "select * from persona";
+    # $resultado = obtenerRegistros($query);
+    # print_r($resultado);
+
+print("Hola Mari :*");
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+    echo "GET";
+    http_response_code(200);
+} else if($_SERVER['REQUEST_METHOD']=='POST'){
+    echo "POST";
+    http_response_code(200);
+}else {
+    http_response_code(405);
+}
+
 ?>
