b9e3566c774eea4774c213bd23418f6afdfd4c0f
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-25 00:25:46 -0400

    metodo post

--- index.php
+++ index.php
@@ -7,34 +7,43 @@
 #print("Hola Mari :*");
 require_once('utilidades.php');
 
-if($_SERVER['REQUEST_METHOD']=='GET'){
-    #echo "GET";
+if(isset($_GET['url'])){
+    
+    if($_SERVER['REQUEST_METHOD']=='GET'){
+        #echo "GET";
 
-    $urlRoute = $_GET['url'];
-    #print($urlRoute);
-    $CI = intval(preg_replace('/[^0-9]+/','',$urlRoute),10);
-    #print($numero);
+        $urlRoute = $_GET['url'];
+        #print($urlRoute);
+        $CI = intval(preg_replace('/[^0-9]+/','',$urlRoute),10);
+        #print($numero);
 
-    switch($urlRoute){
-        case "personas": 
-            $res = obtenerPersonas();
-            #print('{ "vector":');
-            print_r( json_encode($res) );
-            #print('}');
-        case "personas/$CI": 
-            $res = obtenerPersonasCi($CI);
-            #print('{ "vector":');
-            print_r( json_encode($res) );
-            #print('}');
-        default ;
+        switch($urlRoute){
+            case "personas": 
+                $res = obtenerPersonas();
+                #print('{ "vector":');
+                print_r( json_encode($res) );
+                http_response_code(200);
+                #print('}');
+            case "personas/$CI": 
+                $res = obtenerPersonasCi($CI);
+                #print('{ "vector":');
+                print_r( json_encode($res) );
+                http_response_code(200);
+                #print('}');
+            default ;
+        }
+
+        http_response_code(200);
+    } else if($_SERVER['REQUEST_METHOD']=='POST'){
+        $postBody = file_get_contents("php://input");
+        print_r( json_decode($postBody) );
+        http_response_code(200);
+    }else {
+        http_response_code(405);
     }
-
-    http_response_code(200);
-} else if($_SERVER['REQUEST_METHOD']=='POST'){
-    echo "POST";
-    http_response_code(200);
-}else {
-    http_response_code(405);
+} else{
+    //
 }
+
 
 ?>
